895d1921904eba3d4611f35ef498930ee7a04714
Felix Mendez <mendez.felixrn@gmail.com>
2023-01-28 23:34:26 -0500

    Add react router DOM

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <>
-      
+       <Routes>
+       </Routes>
     </>
   );
 }
