a452b8d9395bebe62d585e6cdf930a1d00e31897
Felix Mendez <mendez.felixrn@gmail.com>
2023-02-03 18:48:51 -0500

    Add styling to homepage

--- src/components/MapPreview.js
+++ src/components/MapPreview.js
@@ -1,14 +1,34 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const MapPreview = props => {
     return (
         <Link to="/" style={{color: "black", textDecoration: "none"}}>
-            <div>
-                <h3>{props.name}</h3>
-                <img src={props.image} alt={props.name}/>
-            </div>
+            <Container>
+                <MapName>{props.name}</MapName>
+                <Image src={props.image} alt={props.name}/>
+            </Container>
         </Link>
     )
 }
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 0.5rem 0.5rem;
+    background: rgb(30, 0, 10);
+`;
+
+const MapName = styled.h3`
+    color: white;
+    font-size: 1.25rem;
+    font-family: "special-elite"
+`
+
+const Image = styled.img`
+    width: 100%;
+`
 
 export default MapPreview;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,21 +4,21 @@ import MapPreview from "../components/MapPreview";
 const Home = () => {
     return (
         <>
-            <div>
-                <p>Leaderboard</p>
+            <div className="navbar">
                 <h1>Sought of Hidden</h1>
-                <p>Contact</p>
             </div>
-            <main className="homePage">
-              {maps.map((map) => {
-                return (
-                    <MapPreview 
-                        name={map.name}
-                        image={map.image}
-                        key={map.id}
-                    />
-                )
-              })}
+            <main className="homepage">
+                <div className="gridContainer">
+                  {maps.map((map) => {
+                    return (
+                        <MapPreview 
+                            name={map.name}
+                            image={map.image}
+                            key={map.id}
+                        />
+                    )
+                  })}
+                </div>
             </main>
         </>
     )
